ee32f99eeca27e75a7e40f6375a230ffb31ba42c
Nicole Kinsella <nkinsell@udel.edu>
2024-03-02 13:09:17 -0500

    Created Site 1.2

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                HenHacks24 Project. Hello World!!
+                HenHacks24 Project. Hello World!
             </header>
+            <h5>Hi Users</h5>
         </div>
     );
 }
