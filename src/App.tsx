6d95bc39ee283b046287d74fdb82e5f7bbb5bc68
Nicole Kinsella <nkinsell@udel.edu>
2024-03-02 12:44:54 -0500

    Created Site

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React /*, { useState, useEffect } */ from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">HenHacks24 Project</header>
+        </div>
+    );
+}
+
+export default App;
